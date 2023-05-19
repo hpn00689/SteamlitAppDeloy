cb5081f586dfa11ba1a0e27e228fb56576ae0b8d
hpn00689 <phuocnguyen1062002@gmail.com>
2023-05-20 04:57:30 +0700

    Hello

--- app_build/pages/🔮_Model_and_Linear_Regression.py
+++ app_build/pages/🔮_Model_and_Linear_Regression.py
@@ -1,9 +1,31 @@
 import pandas as pd
 import re
 import numpy as np
+import streamlit as st
 
 from scipy.stats import zscore
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression, LogisticRegression
 from sklearn.metrics import mean_squared_error, mean_absolute_error
+
+# Config trang -----------------------------------------
+st.set_page_config(
+    page_title="Model and Linear Regression",
+    page_icon="🔮",
+    layout="wide"
+)
+
+# Tạo tiêu đề -----------------------------------------
+col1, col2, col3 = st.columns([1,5,1])
+
+with col1:
+    st.write("")
+with col2:
+    st.image('https://i.pinimg.com/564x/b4/10/1e/b4101eb5bc27a62b8f681bd03da9ffff.jpg')
+    st.markdown("<h1 style='text-align: center; color: #B799FF;'>MODEL AND LINEAR REGRESSION</h1>", unsafe_allow_html=True)
+    st.markdown("---", unsafe_allow_html=True)
+with col3:
+    st.write("")
+
+    
 
