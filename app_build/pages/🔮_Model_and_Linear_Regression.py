555641a7cd1afa949dc2efbbaa0ca47035150524
hpn00689 <phuocnguyen1062002@gmail.com>
2023-05-20 04:50:40 +0700

    Hello

--- app_build/pages/🔮_Model_and_Linear_Regression.py
+++ app_build/pages/🔮_Model_and_Linear_Regression.py
@@ -6,3 +6,4 @@ from scipy.stats import zscore
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression, LogisticRegression
 from sklearn.metrics import mean_squared_error, mean_absolute_error
+
